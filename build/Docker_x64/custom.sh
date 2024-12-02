995ab51e5880d8c8fa1b024af0664cc6c096249f
aoxiyu <127184306+aoxiyu@users.noreply.github.com>
2024-12-02 21:36:19 +0800

    Update custom.sh

--- build/Docker_x64/custom.sh
+++ build/Docker_x64/custom.sh
@@ -11,6 +11,7 @@ cat feeds.conf.default
 
 # 添加第三方软件包
 git clone https://github.com/db-one/dbone-packages.git -b 23.05 package/dbone-packages
+git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
 
 # 更新并安装源
 ./scripts/feeds clean
